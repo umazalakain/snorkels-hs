5032b6e7bfc88c10d91d5b9165e3a82504fe0be0
Linus Heckemann <linus.heckemann.2014@uni.strath.ac.uk>
2016-08-18 22:10:45 +0100

    Clean up code without changing functionality

--- snorkels.hs
+++ snorkels.hs
@@ -33,7 +33,8 @@ toString p = ['[', toChar p, ']']
 
 instance Show Board where
     show b = intercalate "\n"
-             [concat [toString (Map.lookup (x, y) (pieces b)) | x <- [0..(fst (size b))]] | y <- [0..(snd (size b))]]
+             [concat [toString (Map.lookup (x, y) (pieces b)) | x <- [0..width]] | y <- [0..height]]
+             where (width, height) = (size b)
 
 inRange :: (Int, Int) -> Int -> Bool
 inRange (min, max) check = min <= check && check < max
@@ -41,11 +42,12 @@ inRange (min, max) check = min <= check && check < max
 inBounds :: (Int, Int) -> Position -> Bool
 inBounds (maxX, maxY) (x, y) = inRange (0, maxX) x && inRange (0, maxY) y
 
-offset :: Position -> (Int, Int) -> Position
+offset :: (Int, Int) -> Position -> Position
 offset (x, y) (x2, y2) = (x+x2, y+y2)
 
 neighbours :: (Int, Int) -> Position -> Set.Set Position
-neighbours bounds pos = Set.fromList (filter (inBounds bounds) (map (offset pos) [(-1, 0), (1, 0), (0, -1), (0, 1)]))
+neighbours bounds pos = Set.fromList (filter (inBounds bounds) (map ((flip offset) pos) neighbourOffsets))
+                        where neighbourOffsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 getGroups :: Board -> Set.Set Group
 getGroups _ = Set.empty
@@ -62,8 +64,10 @@ hasLost b p = any (isTrapped b) (Set.toList (playersGroups b p))
 
 
 sampleBoard :: Board
-sampleBoard = Board { pieces = (Map.insert (0, 0) (Snorkel Green) .
-                                Map.insert (1, 1) (Snorkel Purple) .
-                                Map.insert (5, 3) Stone $
-                                Map.empty)
+sampleBoard = Board { pieces = (Map.fromList [((0, 0), Snorkel Green),
+                                              ((0, 1), Snorkel Green),
+                                              ((0, 3), Snorkel Green),
+                                              ((0, 2), Snorkel Purple),
+                                              ((1, 2), Snorkel Purple),
+                                              ((2, 4), Stone)])
                     , size = (10, 10)}
